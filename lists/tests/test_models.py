f7e9e9a89ab94920c9c94191fbbb77df9ed7dde1
[Michael Mostachetti] <[mjmostachetti@gmail.com]>
2014-09-02 15:54:47 -0700

    Implement duplicate item validation at model layer

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -2,9 +2,8 @@ from django.test import TestCase
 from django.core.exceptions import ValidationError
 from lists.models import List, Item
 
-
-class ListAndItemModelsTest(TestCase):
-
+class ItemModelTest(TestCase):
+    """
     def test_saving_and_retrieving_items(self):
         list_ = List()
         list_.save()
@@ -31,6 +30,28 @@ class ListAndItemModelsTest(TestCase):
         self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, 'Item the second')
         self.assertEqual(second_saved_item.list, list_)
+    """
+
+    def test_default_text(self):
+        item = Item()
+        self.assertEqual(item.text, '')
+
+    def test_item_is_related_to_list(self):
+        list_ = List.objects.create()
+        item = Item()
+        item.list = list_
+        item.save()
+        self.assertIn(item, list_.item_set.all())
+
+    def test_duplicate_items_are_invalid(self):
+        list_ = List.objects.create()
+        Item.objects.create(list=list_, text='bla')
+        with self.assertRaises(ValidationError):
+            item = Item(list=list_, text='bla')
+            item.full_clean()
+            #item.save()
+
+class ListsModelTest(TestCase):
 
     def test_cannot_save_empty_list_items(self):
         list_ = List.objects.create()
@@ -42,4 +63,33 @@ class ListAndItemModelsTest(TestCase):
     def test_get_absolute_url(self):
         list_ = List.objects.create()
         self.assertEqual(list_.get_absolute_url(), '/lists/%d/' % (list_.id,))
+
+
+
+    def test_CAN_save_same_item_to_different_lists(self):
+        list1 = List.objects.create()
+        list2 = List.objects.create()
+        Item.objects.create(list = list1, text = 'bla')
+        item = Item(list=list2, text='bla')
+        item.full_clean() #should not 
+
+    def test_list_ordering(self):
+        list1 = List.objects.create()
+        item1 = Item.objects.create(list=list1, text='i1')
+        item2 = Item.objects.create(list=list1, text='item 2')
+        item3 = Item.objects.create(list=list1, text='3')
+        self.assertEqual(
+            list(Item.objects.all()),
+            [item1, item2, item3]
+        )
+
+    def test_string_representation(self):
+        item = Item(text='some text')
+        self.assertEqual(str(item), 'some text')
+
+
+
+
+
+
 
